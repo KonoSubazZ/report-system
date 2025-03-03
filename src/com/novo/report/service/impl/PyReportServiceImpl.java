b9a669e2bf9063d9b1b0bcd1bcf151995867d68b
levi <warwolf1573@gmail.com>
2025-03-03 11:21:40 +0800

    fix: 修复产品描述不能正常输出

--- src/com/novo/report/service/impl/PyReportServiceImpl.java
+++ src/com/novo/report/service/impl/PyReportServiceImpl.java
@@ -3892,11 +3892,11 @@ public class PyReportServiceImpl implements PyReportService {
         String productDesc1Str = productDesc1.getProduct_desc();
         String toRemove = "";
         if (StringUtils.isEmpty(cancerInfo.get("urinaryProstateDisease").toString())) {
-            toRemove = "内分泌治疗和神经内分泌分化分型以及疾病预后靶向药物、";
+            toRemove = "内分泌治疗和神经内分泌分化分型以及疾病预后、";
             productDesc1Str = productDesc1Str.replace(toRemove, "");
         }
         if (!(boolean) cancerInfo.get("endometrialCarcinoma")) {
-            toRemove = "子宫内膜癌 TCGA 分子分型、";
+            toRemove = "子宫内膜癌TCGA分子分型、";
             productDesc1Str = productDesc1Str.replace(toRemove, "");
         }
         if (!(boolean) cancerInfo.get("gastrointestinalStromalTumor")) {
