f8b570536508c0b8f56590ba746cb7dd16ae89a9
levi <warwolf1573@gmail.com>
2025-03-07 15:23:28 +0800

    feat：更新common_note 字段

--- src/com/novo/report/mod/ModCommonNote.java
+++ src/com/novo/report/mod/ModCommonNote.java
@@ -5,104 +5,121 @@ import java.sql.Timestamp;
 
 public class ModCommonNote {
 
-  private Integer id;
-  private String module;
-  private String type;
-  private String sampleType;
-  private String note;
-  private Integer deleted;
-  private String creator;
-  private java.sql.Timestamp createTime;
-  private String updater;
-  private java.sql.Timestamp updateTime;
+    private Integer id;
+    private String module;
+    private String type;
+    private String sampleType;
+    private String cancer;
+    private String note;
+    private String cancerTitle;
+    private Integer deleted;
+    private String creator;
+    private java.sql.Timestamp createTime;
+    private String updater;
+    private java.sql.Timestamp updateTime;
 
 
-  public Integer getId() {
-    return id;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-
-  public String getModule() {
-    return module;
-  }
-
-  public void setModule(String module) {
-    this.module = module;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
 
-  public String getType() {
-    return type;
-  }
+    public String getModule() {
+        return module;
+    }
 
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public String getSample_type() {
-    return sampleType;
-  }
-
-  public void setSample_type(String sampleType) {
-    this.sampleType = sampleType;
-  }
+    public void setModule(String module) {
+        this.module = module;
+    }
 
 
-  public String getNote() {
-    return note;
-  }
+    public String getType() {
+        return type;
+    }
 
-  public void setNote(String note) {
-    this.note = note;
-  }
+    public void setType(String type) {
+        this.type = type;
+    }
 
+    public String getSample_type() {
+        return sampleType;
+    }
 
-  public Integer getDeleted() {
-    return deleted;
-  }
+    public void setSample_type(String sampleType) {
+        this.sampleType = sampleType;
+    }
 
-  public void setDeleted(Integer deleted) {
-    this.deleted = deleted;
-  }
+    public String getCancer() {
+        return cancer;
+    }
 
+    public void setCancer(String cancer) {
+        this.cancer = cancer;
+    }
 
-  public String getCreator() {
-    return creator;
-  }
+    public String getNote() {
+        return note;
+    }
 
-  public void setCreator(String creator) {
-    this.creator = creator;
-  }
+    public void setNote(String note) {
+        this.note = note;
+    }
 
+    public String getCancer_title() {
+        return cancerTitle;
+    }
 
-  public Timestamp getCreate_time() {
-    return createTime;
-  }
-
-  public void setCreate_time(Timestamp createTime) {
-    this.createTime = createTime;
-  }
-
-
-  public String getUpdater() {
-    return updater;
-  }
-
-  public void setUpdater(String updater) {
-    this.updater = updater;
-  }
+    public void setCancer_title(String cancerTitle) {
+        this.cancerTitle = cancerTitle;
+    }
 
 
-  public Timestamp getUpdate_time() {
-    return updateTime;
-  }
+    public Integer getDeleted() {
+        return deleted;
+    }
 
-  public void setUpdate_time(Timestamp updateTime) {
-    this.updateTime = updateTime;
-  }
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+
+    public Timestamp getCreate_time() {
+        return createTime;
+    }
+
+    public void setCreate_time(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+
+    public Timestamp getUpdate_time() {
+        return updateTime;
+    }
+
+    public void setUpdate_time(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
 
 }
