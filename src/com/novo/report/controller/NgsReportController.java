00f0cce5f18d917c9c07e796245ee2aec1b02d3c
levi <warwolf1573@gmail.com>
2025-04-14 11:27:52 +0800

    feat: 注释送检单位

--- src/com/novo/report/controller/NgsReportController.java
+++ src/com/novo/report/controller/NgsReportController.java
@@ -256,7 +256,7 @@ public class NgsReportController {
             copyto = new String[ccSet.size()];
             ccSet.toArray(copyto);
             //主题
-//            String subject = "请查收诺禾致源的检测报告，姓名：" + sf.getPerson_name() + "-" + sf.getSubbarcode() + ", 送检单位：" + sf.getCustomer();
+//          String subject = "请查收诺禾致源的检测报告，姓名：" + sf.getPerson_name() + "-" + sf.getSubbarcode() + ", 送检单位：" + sf.getCustomer();
             // 20250412取消送检单位
             String subject = "请查收诺禾致源的检测报告，姓名：" + sf.getPerson_name() + "-" + sf.getSubbarcode();
             //内容
