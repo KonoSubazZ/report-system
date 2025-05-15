96c3ef6584820f141953f96e7ac22b9683b6a9c9
levi <warwolf1573@gmail.com>
2025-05-15 11:32:23 +0800

    feat: 更新突变类型v1

--- src/com/novo/report/service/impl/PyReportServiceImpl.java
+++ src/com/novo/report/service/impl/PyReportServiceImpl.java
@@ -4666,7 +4666,7 @@ public class PyReportServiceImpl implements PyReportService {
      * @param ExonicFunc
      * @return
      */
-    @Override
+   /* @Override
     public String translateMutType(String ExonicFunc) {
         switch (ExonicFunc) {
             case "nonsynonymous SNV":
@@ -4698,8 +4698,40 @@ public class PyReportServiceImpl implements PyReportService {
             default:
                 return ExonicFunc;
         }
+    }*/
+    @Override
+    public String translateMutType(String ExonicFunc) {
+        switch (ExonicFunc) {
+            case "nonsynonymous SNV":
+                return "错义突变";
+            case "synonymous SNV":
+                return "同义突变";
+            case "nonframeshift insertion":
+                return "非移码插入";
+            case "nonframeshift deletion":
+                return "非移码缺失";
+            case "frameshift deletion":
+                return "移码缺失";
+            case "frameshift insertion":
+                return "移码插入";
+            case "frameshift indel":
+                return "移码突变";
+            case "nonframeshift indel":
+                return "非移码突变";
+            case "stopgain":
+                return "无义突变";
+            case "stoploss":
+                return "终止子缺失";
+            case "splicing":
+                return "剪接突变";
+            case "promoter":
+                return "启动子区变异";
+            case "unknown":
+                return "未知";
+            default:
+                return ExonicFunc;
+        }
     }
-
     /**
      * 翻译临床意义 12345->是否致病
      *
