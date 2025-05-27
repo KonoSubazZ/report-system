1543a80817cc9e7d7981b54ed71ea22a624f09cb
levi <warwolf1573@gmail.com>
2025-05-27 16:31:23 +0800

    fix: 增加type默认值

--- src/com/novo/report/utils/generate_report.py
+++ src/com/novo/report/utils/generate_report.py
@@ -528,7 +528,7 @@ def mark_genes_in_red(gene_tables, detected_gene_info):
                 detected_gene_list.extend(detected_gene_info.get(key.strip(), []))
         else:
              # 正常处理
-            detected_gene_list = detected_gene_info.get(detected_mapping[gene_type], [])
+            detected_gene_list = detected_gene_info.get(detected_mapping.get(gene_type, ""), [])
 
         # detected_gene_list = detected_gene_info.get(detected_mapping.get(gene_type, ""), [])
         # if not detected_gene_list:
