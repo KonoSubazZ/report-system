c804510d6b8f370b09e0f8e396a28306a2548572
levi <warwolf1573@gmail.com>
2025-05-28 18:01:10 +0800

    fix: 过滤未检出基因

--- src/com/novo/report/service/impl/PyReportServiceImpl.java
+++ src/com/novo/report/service/impl/PyReportServiceImpl.java
@@ -98,9 +98,6 @@ public class PyReportServiceImpl implements PyReportService {
 
     @Autowired
     private DiseaseService diseaseService;
-    @Autowired
-    private DiseaseServiceImpl diseaseServiceImpl;
-
     public static String isAddSymbol(String drug_name_chinese, String cfda, List<Map> clinicalList) {
         List<String> drugNameChineseAll = new ArrayList<String>();
         boolean flag = false;
@@ -3177,7 +3174,7 @@ public class PyReportServiceImpl implements PyReportService {
 
         // 甲状腺癌热点基因检测结果(甲状腺癌)
         List<String> thyroidPanelList = moduleService.getconfPanelList("MOD_WITH_THYROID");
-        boolean isThyroidPanel = thyroidPanelList.contains(productName) && diseaseServiceImpl.isThyroidCarcinoma(diseaseId);
+        boolean isThyroidPanel = thyroidPanelList.contains(productName) && diseaseService.isThyroidCarcinoma(diseaseId);
         String peDrugStr = "";
         if (rt.getTemplate_name().contains("甲状腺") || isThyroidPanel) {
             List<MmThyroidHotspot> thyroidCancerHotAllGeneDrugTipLineStr = moduleModificationAllDao.selectMmThyroidHotspotByReportId(currentNgsAvailable.getReport_id());
