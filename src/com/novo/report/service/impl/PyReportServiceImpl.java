5d6e6a7e1958dcd59259db9a1554b0e497cfd503
levi <warwolf1573@gmail.com>
2025-07-02 15:30:13 +0800

    fix: EGFR突变阳性 + MET Amplification

--- src/com/novo/report/service/impl/PyReportServiceImpl.java
+++ src/com/novo/report/service/impl/PyReportServiceImpl.java
@@ -1203,7 +1203,7 @@ public class PyReportServiceImpl implements PyReportService {
                                 comutation = "ERBB2 Amplification + KRAS&NRAS&BRAF野生型";
                             } else if (ori_variant.equals("ERBB2 Amplification + KRAS WildType + NRAS WildType")) {
                                 comutation = "ERBB2 Amplification + KRAS&NRAS野生型";
-                            }else if (ori_variant.equals("EGFR Sensitizing Mutation + MET Amplification")) {
+                            } else if (ori_variant.equals("EGFR Sensitizing Mutation + MET Amplification")) {
                                 comutation = "EGFR突变阳性 + MET Amplification";
                             }
                             targetDrugTipLine.put("comutation", comutation);
@@ -1907,6 +1907,8 @@ public class PyReportServiceImpl implements PyReportService {
                             comutation = "ERBB2 Amplification + KRAS&NRAS&BRAF野生型";
                         } else if (ori_variant.equals("ERBB2 Amplification + KRAS WildType + NRAS WildType")) {
                             comutation = "ERBB2 Amplification + KRAS&NRAS野生型";
+                        } else if (ori_variant.equals("EGFR Sensitizing Mutation + MET Amplification")) {
+                            comutation = "EGFR突变阳性 + MET Amplification";
                         }
                         targetedDrugDetection.put("comutation", comutation);
                         complexDrugStr.add(targetedDrugDetection);
