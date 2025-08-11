d2913f93b652c48cc63aa0e10bbe690280a025f2
levi <warwolf1573@gmail.com>
2025-08-11 14:47:09 +0800

    feat: 改为修改响应体

--- src/com/novo/report/controller/SubreportController.java
+++ src/com/novo/report/controller/SubreportController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -15,6 +16,7 @@ public class SubreportController {
     @Autowired
     private SubReportService subReportService;
     @RequestMapping("/gen/{id}")
+    @ResponseBody
     public String generateSubreport(@PathVariable("id") Integer reportId)
     {
         return subReportService.generateSubReport(reportId);
