72b00ba8380b876adb2f1b0ef418329c2ef2f962
levi <warwolf1573@gmail.com>
2025-11-28 15:08:04 +0800

    fix: 判断错误

--- src/com/novo/report/service/impl/PyReportServiceImpl.java
+++ src/com/novo/report/service/impl/PyReportServiceImpl.java
@@ -4797,7 +4797,7 @@ public class PyReportServiceImpl implements PyReportService {
             toRemove = "、免疫药物";
             productDesc1Str = productDesc1Str.replace(toRemove, "");
         }
-        if ((!diseaseInfo.get("ThyroidCarcinoma") || !conf.getThyroid_cancer_prognosis()) && (!conf.getMelanoma_prognosis())) {
+        if ((!diseaseInfo.get("ThyroidCarcinoma") || !conf.getThyroid_cancer_prognosis()) && (!diseaseInfo.get("Melanoma") || !conf.getMelanoma_prognosis())) {
             toRemove = "、预后评估";
             productDesc1Str = productDesc1Str.replace(toRemove, "");
         }
