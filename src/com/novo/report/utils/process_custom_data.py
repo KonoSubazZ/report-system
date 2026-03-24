bf21199fd9fcccf727265f8e9f6fe8d01c56a2f6
levi <warwolf1573@gmail.com>
2026-03-24 15:06:59 +0800

    feat: 更新上海肺科位点分类逻辑

--- src/com/novo/report/utils/process_custom_data.py
+++ src/com/novo/report/utils/process_custom_data.py
@@ -124,7 +124,10 @@ def process_shanghaifeike_tip(report_json):
             var = new_variant[c_idx:]
             tip = f"{gene}基因{exon}号{desc}{ExonicFunc}{var}，突变丰度为{mut_freq}。"
 
-        if variationClass2 == "1" or (gene == "KRAS" and variationClass2 == "2") or (gene == "KRAS" and ori_variant != "Amplification"):
+        if (gene == "EGFR" and ori_variant == "Amplification"):
+            shanghaifeike_tips_2.append(tip)
+            item['variationClass'] = "II类"
+        elif variationClass2 == "1" or (gene == "KRAS" and ori_variant != "Amplification"):
             item['variationClass'] = "I类"
             shanghaifeike_tips_1.append(tip)
         else:
